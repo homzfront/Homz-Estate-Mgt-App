10634ea4fae60ff56621c6025646cbfcf965383a
lordyemight <lordyemight@gmail.com>
2025-07-22 15:07:09 +0100

    spacing

--- src/store/useUserStore.ts
+++ src/store/useUserStore.ts
@@ -1,9 +1,11 @@
 import { create } from 'zustand';
 
+
 interface UserStore {
   userData: boolean;
   setUserData: (value: boolean) => void;
 }
+
 
 export const useUserStore = create<UserStore>((set) => ({
   userData: false,
