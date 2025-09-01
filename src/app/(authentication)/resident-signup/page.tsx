d029062f990c81c03a655b3afec1bc5b23e0b462
LordYemight <lordyemight@gmail.com>
2025-09-01 04:10:32 +0100

    work

--- src/app/(authentication)/resident-signup/page.tsx
+++ src/app/(authentication)/resident-signup/page.tsx
@@ -18,19 +18,20 @@ const ResidentSignup = () => {
   // Handle URL parameters
   useResidentParams()
 
-  const getPublicEstate = async () => {
-    try {
-      const response = await api.get(
-        `/api/v1/estates/public/single-estate/organizations/${organizationId}/estates/${estateId}`
+  // const getPublicEstate = async () => {
+  //   try {
+  //     const response = await api.get(
+  //       `/api/v1/estates/public/single-estate/organizations/${organizationId}/estates/${estateId}`
 
-      );
+  //     );
 
-      setPublicCommunity(response?.data?.data?.estates);
-    } catch (error) {
-      console.error("Failed to fetch estates:", error);
-    };
-  };
+  //     setPublicCommunity(response?.data?.data?.estates);
+  //   } catch (error) {
+  //     console.error("Failed to fetch estates:", error);
+  //   };
+  // };
 
+  console.log(organizationId, estateId);
   return (
     <div className="flex m-auto max-w-[100%] sm:max-w-[1440px] h-[1024px]">
       <div className="w-[644px] hidden lg:flex flex-col py-8 justify-around bg-[url('/Background_image2.png')] bg-BlueHomz">
